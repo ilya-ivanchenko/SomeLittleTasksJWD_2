18e94d28c776ae1b891a00fea1b7e6c61d7e837b
ivanchenko92 <richiilya92@gmail.com>
2022-02-15 22:03:43 +0300

    ок

---
+++ src/by/epam/JWD/step2/Input.java
@@ -0,0 +1,47 @@
+package  by.epam.JWD.step2;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Input {
+
+    public int inputRandom() {
+        int number;
+        Random random = new Random();
+        number = random.nextInt(10);
+        return number;
+    }
+
+    public int[] inputScannerArray() {
+        Scanner  sc = new Scanner(System.in);
+        System.out.println("Введите размерность массива: ");
+        while (!sc.hasNextDouble()) {
+            sc.next();
+            System.out.print("Вводите только целые числа: ");
+        }
+        int s = sc.nextInt();
+        int arr[] = new int[s];
+
+        for (int i=0; i<arr.length; i++) {
+            System.out.print("Введите "  + i  + " элемент массива: ");
+            while (!sc.hasNextDouble()) {
+                sc.next();
+                System.out.print("Вводите только целые числа: ");
+            }
+            arr[i]  = sc.nextInt();
+        }
+        return  arr;
+    }
+
+    public int[][] inputScanner() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите размер матрицы n*n: ");
+        while (!sc.hasNextDouble()) {
+            sc.next();
+            System.out.print("Вводите только целые числа: ");
+        }
+        int s = sc.nextInt();
+        int array[][] = new int[s][s];
+        return array;
+    }
+}
---
+++ src/by/epam/JWD/step2/Main.java
@@ -0,0 +1,38 @@
+package  by.epam.JWD.step2;
+
+public class Main {
+    public static void main(String[] args) {
+        Input input = new Input();
+        Logic logic = new Logic();
+        View view = new View();
+
+        /* task4 */
+        int x = 0;
+        int y = -1;
+        boolean check =  logic.pointBelong(x,y);
+        view.showIsTrue(check);
+
+        /* task2 */
+        int a = input.inputRandom();
+        int b = input.inputRandom();
+        int c = input.inputRandom();
+        double result = logic.formula(a, b, c);
+        view.showResult(result);
+
+        /* task9 */
+        int k = 3;
+        int array1[] = input.inputScannerArray();
+        view.showArr(array1);
+        int array2[] = input.inputScannerArray();
+        view.showArr(array2);
+        int array3[] = logic.putArray(array1, array2, k);
+        view.showArr(array3);
+
+        /* task10*/
+        int[][] array = input.inputScanner();
+        int[][] arrayNew = logic.sortArray(array);
+        view.showArrBig(arrayNew);
+
+
+    }
+}
